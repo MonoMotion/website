42cc10c4ad57581b690fed2996fd114a485fe4dd
coord.e <me@coord-e.com>
2019-02-13 23:10:29 +0800

    [WIP] Add: Start adding hero elemenr

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import 'react-bulma-components/dist/react-bulma-components.min.css'
 
 import Header from './Header'
+import Hero from './Hero'
 
 
 const App = () => (
   <div className="App">
     <Header />
+    <main role="main">
+      <Hero />
+    </main>
   </div>
 )
 
