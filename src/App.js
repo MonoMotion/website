8f3490062187f5e39920a535cb8eea9a25441fdd
coord.e <me@coord-e.com>
2019-02-13 23:03:42 +0800

    Change: Separate header component

--- src/App.js
+++ src/App.js
@@ -1,72 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Button, Navbar } from 'react-bulma-components';
+
+import Header from './header';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <Navbar role="navigation" aria-label="main navigation">
-            <Navbar.Brand>
-              <Navbar.Item href="https://bulma.io">
-                MonoMotion
-              </Navbar.Item>
-
-              <Navbar.Burger role="button" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-              </Navbar.Burger>
-            </Navbar.Brand>
-
-            <Navbar.Menu id="navbarBasicExample">
-              <Navbar.Item>
-                About
-              </Navbar.Item>
-
-              <Navbar.Item>
-                Documentation
-              </Navbar.Item>
-
-              <Navbar.Item className="has-dropdown is-hoverable">
-                <Navbar.Link>
-                  Technology
-                </Navbar.Link>
-
-                <Navbar.Dropdown>
-                  <Navbar.Item>
-                    Reficere
-                  </Navbar.Item>
-                  <Navbar.Item>
-                    flom
-                  </Navbar.Item>
-                  <Navbar.Item>
-                    servoarray
-                  </Navbar.Item>
-                  <Navbar.Item>
-                    Trainer
-                  </Navbar.Item>
-                </Navbar.Dropdown>
-              </Navbar.Item>
-
-              <Navbar.Item>
-                Contact
-              </Navbar.Item>
-
-              <Navbar.Container position="end">
-                <Navbar.Item>
-                  <Button className="is-primary">
-                    Try now
-                  </Button>
-                </Navbar.Item>
-              </Navbar.Container>
-            </Navbar.Menu>
-          </Navbar>
-        </header>
+        <Header />
       </div>
     );
   }
---
+++ src/header.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Button, Navbar } from 'react-bulma-components';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <Navbar role="navigation" aria-label="main navigation">
+          <Navbar.Brand>
+            <Navbar.Item href="https://bulma.io">
+              MonoMotion
+            </Navbar.Item>
+
+            <Navbar.Burger role="button" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </Navbar.Burger>
+          </Navbar.Brand>
+
+          <Navbar.Menu id="navbarBasicExample">
+            <Navbar.Item>
+              About
+            </Navbar.Item>
+
+            <Navbar.Item>
+              Documentation
+            </Navbar.Item>
+
+            <Navbar.Item className="has-dropdown is-hoverable">
+              <Navbar.Link>
+                Technology
+              </Navbar.Link>
+
+              <Navbar.Dropdown>
+                <Navbar.Item>
+                  Reficere
+                </Navbar.Item>
+                <Navbar.Item>
+                  flom
+                </Navbar.Item>
+                <Navbar.Item>
+                  servoarray
+                </Navbar.Item>
+                <Navbar.Item>
+                  Trainer
+                </Navbar.Item>
+              </Navbar.Dropdown>
+            </Navbar.Item>
+
+            <Navbar.Item>
+              Contact
+            </Navbar.Item>
+
+            <Navbar.Container position="end">
+              <Navbar.Item>
+                <Button className="is-primary">
+                  Try now
+                </Button>
+              </Navbar.Item>
+            </Navbar.Container>
+          </Navbar.Menu>
+        </Navbar>
+      </header>
+    );
+  }
+}
+
+export default Header;
