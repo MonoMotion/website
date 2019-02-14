bd4ddac3c629e23b02ffa03765d3f63682cb85b8
coord.e <me@coord-e.com>
2019-02-14 23:02:20 +0800

    Add: Add about section

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import 'react-bulma-components/dist/react-bulma-components.min.css'
 
 import Top from './Top'
 import Timeline from './Timeline'
+import About from './About'
 
 
 const App = () => (
   <div className="App">
     <Top />
+    <About />
     <Timeline />
   </div>
 )
