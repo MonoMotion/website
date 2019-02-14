22074eb85a694baecef5b960d975e2bdacdbad63
coord.e <me@coord-e.com>
2019-02-14 23:09:17 +0800

    Add: Add footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'react-bulma-components/dist/react-bulma-components.min.css'
 import Top from './Top'
 import Timeline from './Timeline'
 import About from './About'
+import Footer from './Footer'
 
 
 const App = () => (
@@ -11,6 +12,7 @@ const App = () => (
     <Top />
     <About />
     <Timeline />
+    <Footer />
   </div>
 )
 
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Content, Footer } from 'react-bulma-components'
+
+const FooterContent = (props) => (
+  <Footer>
+    <Content className="has-text-centered">
+      <p>
+        <strong>MonoMotion</strong> framework by <a href="https://twitter.com/coord_e">@coord_e</a> & <a href="https://twitter.com/Nyanyan_Cube">@Nyanyan_Cube</a>
+      </p>
+    </Content>
+  </Footer>
+)
+
+export default FooterContent
