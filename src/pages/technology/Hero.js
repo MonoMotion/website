601e673765a28c10aa7efe835deafbc91a402280
coord.e <me@coord-e.com>
2019-02-15 07:25:49 +0800

    [WIP] Add Add hero

---
+++ src/pages/technology/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Heading, Hero } from 'react-bulma-components'
+
+const Top = () => (
+  <Hero color="primary" size="medium">
+    <Hero.Body>
+      <Heading>Technology</Heading>
+      <Heading subtitle>イカした技術を紹介するぜ！</Heading>
+    </Hero.Body>
+  </Hero>
+)
+
+export default Top
--- src/pages/technology/index.js
+++ src/pages/technology/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
 import Header from '../../Header'
+import Hero from './Hero'
 
-import { Section } from 'react-bulma-components'
+import { Heading, Section, Tile } from 'react-bulma-components'
 
 const Tech = () => (
-  <div>
-    <Section paddingless>
-      <Header />
-    </Section>
+<div>
+  <Section paddingless>
+    <Header />
+  </Section>
+  <Section paddingless>
+    <Hero />
+  </Section>
   </div>
 )
 
