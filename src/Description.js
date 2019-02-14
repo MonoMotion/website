5ffe32ee37808c0c3938b79495b9aee01d5bd4b9
coord.e <me@coord-e.com>
2019-02-15 00:19:02 +0800

    Add: Enable to switch left or right

--- src/Description.js
+++ src/Description.js
@@ -5,27 +5,36 @@ import BlendMotionImg from './images/blendmotion.png'
 import TrainerImg from './images/trainer.png'
 import RunnerImg from './images/runner.png'
 
-const DescriptionRow = (props) => (
-  <Section>
-    <Container>
-      <Columns>
-        <Columns.Column>
-          {props.hero}
-        </Columns.Column>
-        <Columns.Column>
-          <Heading subtitle>{props.subtitle}</Heading>
-          <Heading>{props.title}</Heading>
-          <p>
-            {props.children}
-          </p>
-          <Button href={props.link}>
-            技術について
-          </Button>
-        </Columns.Column>
-      </Columns>
-    </Container>
-  </Section>
-)
+const DescriptionRow = (props) => {
+  const hero = props.hero
+  const desc = (
+    <div>
+      <Heading subtitle>{props.subtitle}</Heading>
+      <Heading>{props.title}</Heading>
+      <p>
+        {props.children}
+      </p>
+      <Button href={props.link}>
+        技術について
+      </Button>
+    </div>
+  )
+
+  return (
+    <Section>
+      <Container>
+        <Columns>
+          <Columns.Column>
+            {props.left ? desc : hero}
+          </Columns.Column>
+          <Columns.Column>
+            {props.right ? desc : hero}
+          </Columns.Column>
+        </Columns>
+      </Container>
+    </Section>
+  )
+}
 
 const Description = () => (
   <div>
@@ -36,6 +45,7 @@ const Description = () => (
       hero={
         <iframe title="reficere-movie" width="560" height="315" src="https://www.youtube.com/embed/nECBQznYx9g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       }
+      left
     >
       ロボットの3Dモデルを作るにあたって、各パーツの重量測定と重心位置の計算は非常に大変でした。いちいち分解する必要があったからです。Reficereはロボットを分解することなく、短時間に半自動的に重量測定をする重量測定支援システムです。
     </DescriptionRow>
@@ -46,6 +56,7 @@ const Description = () => (
       hero={
         <img src={BlendMotionImg} alt="BlendMotion" />
       }
+      right
     >
       ロボットモデルを使い、ロボットのモーションをデザインするインターフェースを提供します。Blenderという広く使われている3DCGソフトウェアをベースにしており、誰でも簡単にモーションをデザインすることができます。
     </DescriptionRow>
@@ -56,6 +67,7 @@ const Description = () => (
       hero={
         <img src={TrainerImg} alt="Trainer" />
       }
+      left
     >
       デザインしたモーションは、そのまま物理世界で意図した通りに動くとは限りません。Trainerは、モーションが製作者の意図通りに動くように自動でモーションを改善します。
     </DescriptionRow>
@@ -66,6 +78,7 @@ const Description = () => (
       hero={
         <img src={RunnerImg} alt="Runner" />
       }
+      right
     >
       あとは実機のロボットで動かすだけです。flom、servoarrayテクノロジーにより、実機でのモーション実行が今までと比べて圧倒的に簡単かつシンプルになっています。
     </DescriptionRow>
