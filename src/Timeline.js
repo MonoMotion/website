242ef5b07ae696d6472b8b72704470a233cfcaee
coord.e <me@coord-e.com>
2019-02-15 00:27:44 +0800

    Change: Adjust timeline

--- src/Timeline.js
+++ src/Timeline.js
@@ -4,22 +4,17 @@ import 'react-vertical-timeline-component/style.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faWeight, faHandPointer, faMagic, faMicrochip } from '@fortawesome/free-solid-svg-icons'
 
-import { Heading, Section, Button, Container, Icon } from 'react-bulma-components'
+import { Columns, Heading, Section, Button, Container, Icon } from 'react-bulma-components'
 
 
 const TimelineElement = (props) => (
   <VerticalTimelineElement
     iconStyle={{ background: props.background, color: '#fff' }}
     icon={<Icon><FontAwesomeIcon icon={props.icon} /></Icon>}
+    date={<Button>詳しく</Button>}
   >
     <Heading size={4}>{props.title}</Heading>
-    <Heading subtitle size={5}>{props.subtitle}</Heading>
-    <p>
-      {props.children}
-    </p>
-    <Button>
-      Learn more
-    </Button>
+    <p>{props.subtitle}</p>
   </VerticalTimelineElement>
 )
 
@@ -28,43 +23,35 @@ const TimelineSection = () => (
   <Section className="has-background-dark">
     <Container>
       <Heading className="has-text-light">
-        ロボットを動かす流れ
+        MonoMotionは、大きく分けて以下の4ステップで構成されています:
       </Heading>
     </Container>
     <Container>
       <VerticalTimeline layout="one-column">
         <TimelineElement
           title="Reficere"
-          subtitle="モデル作成を早く簡単に。"
+          subtitle="モデル作成を早く簡単に。Reficereはロボットを分解することなく、短時間に半自動的に重量測定をする重量測定支援システムです。"
           background="rgb(33, 150, 243)"
           icon={faWeight}
-        >
-            ロボットの3Dモデルを作るにあたって、各パーツの重量測定と重心位置の計算は非常に大変でした。いちいち分解する必要があったからです。Reficereはロボットを分解することなく、短時間に半自動的に重量測定をする重量測定支援システムです。
-        </TimelineElement>
+        />
         <TimelineElement
           title="BlendMotion"
-          subtitle="モーションをデザインする。"
+          subtitle="モーションをデザインする。ロボットモデルを使い、ロボットのモーションをデザインするインターフェースを提供します。"
           background="rgb(33, 150, 243)"
           icon={faHandPointer}
-        >
-          ロボットモデルを使い、ロボットのモーションをデザインするインターフェースを提供します。Blenderという広く使われている3DCGソフトウェアをベースにしており、誰でも簡単にモーションをデザインすることができます。
-        </TimelineElement>
+        />
         <TimelineElement
           title="Trainer"
-          subtitle="モーションを自動で改善。"
+          subtitle="モーションを自動で改善。モーションが製作者の意図通りに動くように自動でモーションを改善します。"
           background="rgb(33, 150, 243)"
           icon={faMagic}
-        >
-          デザインしたモーションは、そのまま物理世界で意図した通りに動くとは限りません。Trainerは、モーションが製作者の意図通りに動くように自動でモーションを改善します。
-        </TimelineElement>
+        />
         <TimelineElement
           title="flom-runner"
-          subtitle="あとは実機で動かすだけ。"
+          subtitle="あとは実機で動かすだけ。ハードウェア間の差異を開発者、利用者が感じることはありません。"
           background="rgb(33, 150, 243)"
           icon={faMicrochip}
-        >
-          あとは実機のロボットで動かすだけです。flom、servoarrayテクノロジーにより、実機でのモーション実行が今までと比べて圧倒的に簡単かつシンプルになっています。
-        </TimelineElement>
+        />
       </VerticalTimeline>
     </Container>
   </Section>
