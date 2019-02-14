8c07a33885501ecc33f12da2e8823e71e2f6df0d
coord.e <me@coord-e.com>
2019-02-15 00:15:30 +0800

    Add: Add hero image/videos

--- src/Description.js
+++ src/Description.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Heading, Button, Section, Container, Columns } from 'react-bulma-components'
 
+import BlendMotionImg from './images/blendmotion.png'
+import TrainerImg from './images/trainer.png'
+import RunnerImg from './images/runner.png'
+
 const DescriptionRow = (props) => (
   <Section>
     <Container>
@@ -29,6 +33,9 @@ const Description = () => (
       title="Refirece"
       subtitle="モデル作成を早く簡単に。"
       link="/"
+      hero={
+        <iframe title="reficere-movie" width="560" height="315" src="https://www.youtube.com/embed/nECBQznYx9g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      }
     >
       ロボットの3Dモデルを作るにあたって、各パーツの重量測定と重心位置の計算は非常に大変でした。いちいち分解する必要があったからです。Reficereはロボットを分解することなく、短時間に半自動的に重量測定をする重量測定支援システムです。
     </DescriptionRow>
@@ -36,6 +43,9 @@ const Description = () => (
       title="BlendMotion"
       subtitle="モーションをデザインする。"
       link="/"
+      hero={
+        <img src={BlendMotionImg} alt="BlendMotion" />
+      }
     >
       ロボットモデルを使い、ロボットのモーションをデザインするインターフェースを提供します。Blenderという広く使われている3DCGソフトウェアをベースにしており、誰でも簡単にモーションをデザインすることができます。
     </DescriptionRow>
@@ -43,6 +53,9 @@ const Description = () => (
       title="Trainer"
       subtitle="モーションを自動で改善。"
       link="/"
+      hero={
+        <img src={TrainerImg} alt="Trainer" />
+      }
     >
       デザインしたモーションは、そのまま物理世界で意図した通りに動くとは限りません。Trainerは、モーションが製作者の意図通りに動くように自動でモーションを改善します。
     </DescriptionRow>
@@ -50,6 +63,9 @@ const Description = () => (
       title="flom-runner"
       subtitle="あとは実機で動かすだけ。"
       link="/"
+      hero={
+        <img src={RunnerImg} alt="Runner" />
+      }
     >
       あとは実機のロボットで動かすだけです。flom、servoarrayテクノロジーにより、実機でのモーション実行が今までと比べて圧倒的に簡単かつシンプルになっています。
     </DescriptionRow>
