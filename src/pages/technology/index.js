c5f47eef87782f8989b113e73a88fd9adb7b8a3c
coord.e <me@coord-e.com>
2019-02-15 07:59:29 +0800

    [WIP] Add: Add tiles

--- src/pages/technology/index.js
+++ src/pages/technology/index.js
@@ -3,7 +3,8 @@ import React from 'react'
 import Header from '../../Header'
 import Hero from './Hero'
 
-import { Heading, Section, Tile } from 'react-bulma-components'
+import { Content, Heading, Section, Tile } from 'react-bulma-components'
+import { Link } from 'react-router-dom'
 
 const Tech = () => (
 <div>
@@ -13,7 +14,45 @@ const Tech = () => (
   <Section paddingless>
     <Hero />
   </Section>
-  </div>
+  <Section>
+    <Tile kind="ancestor">
+      <Tile size={8} vertical>
+        <Tile>
+          <Tile kind="parent" vertical>
+            <Tile renderAs="a" kind="child" notification color="primary" to="/technology/trainer">
+              <Heading>Trainer</Heading>
+              <Heading subtitle>学習の裏側</Heading>
+            </Tile>
+            <Tile renderAs="a" kind="child" notification color="warning" to="/technology/servoarray">
+              <Heading>servoarray</Heading>
+              <Heading subtitle>モーションがあなたのロボットで動くまで</Heading>
+            </Tile>
+          </Tile>
+          <Tile kind="parent">
+            <Tile renderAs="a" kind="child" notification color="info" to="/technology/flom">
+              <Heading>flom モーションのためのライブラリ</Heading>
+              <Heading subtitle>MonoMotionを裏で支える技術</Heading>
+            </Tile>
+          </Tile>
+        </Tile>
+        <Tile kind="parent">
+          <Tile renderAs="article" kind="child" notification color="danger">
+            <Heading>Coming soon...</Heading>
+            <Heading subtitle>新しい記事を(心が持つ限り)書きます</Heading>
+            <div className="content" />
+          </Tile>
+        </Tile>
+      </Tile>
+      <Tile kind="parent">
+        <Tile renderAs={Link} kind="child" notification color="success" to="/technology/reficere">
+          <Heading>Reficere 詳細</Heading>
+          <Heading subtitle>数式に威圧されたい方に。</Heading>
+          <Content />
+        </Tile>
+      </Tile>
+    </Tile>
+  </Section>
+</div>
 )
 
 export default Tech
