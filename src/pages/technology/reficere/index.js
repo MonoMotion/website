2679ff50bf24988687a8e799670b211818dfb58e
coord.e <me@coord-e.com>
2019-02-16 13:03:17 +0900

    Add: Make a heading hero

--- src/pages/technology/reficere/index.js
+++ src/pages/technology/reficere/index.js
@@ -4,11 +4,20 @@ import Header from '../../../Header'
 /* eslint import/no-webpack-loader-syntax: off */
 import post from '!html-loader!!markdown-loader!./details.md'
 
-import { Content, Heading, Section } from 'react-bulma-components'
+import { Hero, Content, Container, Heading, Section } from 'react-bulma-components'
 
 const Post = () => (
   <div>
     <Header />
+    <Hero color="primary" size="medium">
+      <Hero.Body>
+        <Container>
+          <Heading>
+            Reficere詳解
+          </Heading>
+        </Container>
+      </Hero.Body>
+    </Hero>
     <Section>
       <Content>
         <div dangerouslySetInnerHTML={{ __html: post }} />
